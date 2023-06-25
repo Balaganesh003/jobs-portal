3a23c8b4787164f68594b7f0c49fdee9a0d8b5b9
Balaganesh K <k.balaganesh26@gmail.com>
2023-06-25 11:39:13 +0530

    added the dream careers page

--- pages/admin/companies/[...id]/index.js
+++ pages/admin/companies/[...id]/index.js
@@ -63,7 +63,7 @@ const Companies = () => {
           </div>
         </div>
       </div>
-      <div className="max-w-[62.5rem] w-full min-h-screen h-full mx-auto  mt-5 z-0">
+      <div className="max-w-[62.5rem] w-full min-h-[calc(100vh-9rem)] md:mt-[15px] h-full mx-auto  z-0">
         {/* Cards section */}
 
         {currentActive === '/' && <CompanyPage />}
