0876710539187aa605ddaff926de341abaf39633
Balaganesh K <k.balaganesh26@gmail.com>
2023-06-25 18:47:52 +0530

    updated the filterscard

--- components/FilterCard.jsx
+++ components/FilterCard.jsx
@@ -15,11 +15,11 @@ const FilterCard = ({
       href={`/admin/companies/${companyId}/${link == '/' ? '' : link}`}>
       <div
         className={`px-[14.5px] flex-shrink-0 py-[6px] my-[4px] mr-2 border border-black rounded w-fit h-[35px] ${
-          currentActive == link && ' bg-black'
+          currentActive === link && ' bg-black'
         }`}>
         <p
           className={`text-[0.875rem] leading-[150%] font-semibold text-primary-text captilize cursor-pointer ${
-            currentActive == link && ' text-white'
+            currentActive === link && ' text-white'
           }`}>
           {name}
         </p>
